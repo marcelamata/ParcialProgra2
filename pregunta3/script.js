76e1309921002bbc348dfec3c357cbc138c03d3a
Marcela Mata <marcela.mata01@hotmail.com>
2021-02-23 20:04:43 -0600

    Primer commit

---
+++ pregunta3/script.js
@@ -0,0 +1,7 @@
+function formulario() {
+    var table = document.createElement("table");
+    var compra = document.getElementById("compra");
+    var precio = document.getElementById("precio");
+    var cantidad = document.getElementById("cantidad");
+
+}
