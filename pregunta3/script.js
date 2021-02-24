71e208dbb9b0942ba09eba15daea0469081def71
Marcela Mata <marcela.mata01@hotmail.com>
2021-02-23 21:10:53 -0600

    Quinto commit

--- pregunta3/script.js
+++ pregunta3/script.js
@@ -8,7 +8,7 @@ function formulario() {
     var row = table.insertRow(0);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
-    var cell3 = row.insertRow(2);
+    var cell3 = row.insertCell(2);
     cell1.innerHTML = compra;
     cell2.innerHTML = cantidad;
     cell3.innerHTML = precio;
